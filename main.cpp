be3a35edf85fe9668f03152ba7f02892d812711e
abdullah-ibn-umar <jakha_96-96@mail.ru>
2019-09-12 14:40:13 +0300

    Gitignore changed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-#include ""
+#include "libuv/include/uv.h"
+
+static uv_tcp_t server;
+
 int main() {
+    uv_init();
+    uv_tcp_init(server, NULL);
     std::cout<<"Hello, World!"<<std::endl;
+
+    uv_run();
     return 0;
+
 }

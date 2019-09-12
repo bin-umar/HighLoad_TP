f4ac791909636e3de4fc952c1cf612324c382d69
abdullah-ibn-umar <jakha_96-96@mail.ru>
2019-09-12 23:58:05 +0300

    Fixed library dependency

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <cassert>
-#include "libuv/include/uv.h"
-#include "http-parser/http_parser.h"
+#include "uv.h"
 
 static uv_tcp_t server;
 static uv_loop_t* uv_loop;
-static http_parser_settings parser_settings;
+
+#define DEFAULT_PORT  3000
+#define DEFAULT_BACKLOG 1024
 
 using std::cout;
 using std::endl;
 
 typedef struct {
     uv_tcp_t handle;
-    http_parser parser;
     uv_write_t write_req;
     int request_num;
 } client_t;
 
 void on_alloc(uv_handle_t* client, size_t suggested_size, uv_buf_t* buf) {
+    cout << "alloc_buffer " << suggested_size << endl;
     buf->base = new char(suggested_size);
     buf->len = suggested_size;
 }
@@ -29,41 +30,30 @@ void on_close(uv_handle_t* handle) {
 }
 
 void on_read(uv_stream_t* tcp, ssize_t nread, const uv_buf_t* buf) {
-    size_t parsed;
-    auto client = (client_t*) tcp->data;
-
-    if (nread >= 0) {
-        parsed = http_parser_execute(&client->parser, &parser_settings, buf->base, nread);
-        if (parsed < nread) {
-            std::cerr << "parse error" << endl;
-            uv_close((uv_handle_t*) &client->handle, on_close);
-        }
+    cout << "echo_read(stream=" << tcp << "nread=" << nread << endl;
+    if (nread == UV_EOF) {
+        cout << "<EOF>" << endl;
     }
+    cout << "Buff" << buf->base << endl;
 
     delete buf->base;
 }
 
-static int request_num = 1;
-
 void on_connect(uv_stream_t* server_handle, int status) {
-    assert(status == 0);
     int r;
+
+    assert(status == 0);
     assert((uv_tcp_t*)server_handle == &server);
 
-    auto *client = new client_t;
-    client->request_num = request_num;
-    cout << request_num++ << "new connection" << endl;
+    auto *client = new uv_tcp_t;
+    uv_tcp_init(uv_loop, client);
 
-    uv_tcp_init(uv_loop, &client->handle);
-    http_parser_init(&client->parser, HTTP_REQUEST);
-
-    client->parser.data = client;
-    client->handle.data = client;
-
-    r = uv_accept(server_handle, (uv_stream_t*)&client->handle);
-    assert(r == 0);
-
-    uv_read_start((uv_stream_t*)&client->handle, on_alloc, on_read);
+    r = uv_accept(server_handle, (uv_stream_t*) client);
+    if (r == 0) {
+        uv_read_start((uv_stream_t*) client, on_alloc, on_read);
+    } else {
+        uv_close((uv_handle_t*) client, on_close);
+    }
 }
 
 int main() {
@@ -74,13 +64,13 @@ int main() {
       assert(r == 0);
 
       struct sockaddr_in addr{};
-      r = uv_ip4_addr("0.0.0.0", 8000, &addr);
+      r = uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
       assert(r == 0);
 
       r = uv_tcp_bind(&server, (const struct sockaddr*) &addr, 0);
       assert(r == 0);
 
-      uv_listen((uv_stream_t*) &server, 128, on_connect);
+      uv_listen((uv_stream_t*) &server, DEFAULT_BACKLOG, on_connect);
       cout << "listening on port 8000" << endl;
       return uv_run(uv_loop, UV_RUN_DEFAULT);
 }

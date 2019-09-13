d70cfa1ded63fcbfbdb3f34f896719ea09e9409b
abdullah-ibn-umar <jakha_96-96@mail.ru>
2019-09-13 04:12:58 +0300

    First contact between sockets

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
+#include <sys/socket.h>
 #include <iostream>
 #include <cassert>
+#include <cstring>
 #include "uv.h"
 
 static uv_tcp_t server;
 static uv_loop_t* uv_loop;
 
-#define DEFAULT_PORT  3000
+#define DEFAULT_PORT  8000
 #define DEFAULT_BACKLOG 1024
 
 using std::cout;
@@ -19,24 +21,38 @@ typedef struct {
 
 void on_alloc(uv_handle_t* client, size_t suggested_size, uv_buf_t* buf) {
     cout << "alloc_buffer " << suggested_size << endl;
-    buf->base = new char(suggested_size);
+    buf->base = new char[suggested_size];
     buf->len = suggested_size;
 }
 
 void on_close(uv_handle_t* handle) {
     auto* client = (client_t*) handle->data;
-    cout << client->request_num << "connection closed" << endl;
+    cout << client << "connection closed" << endl;
     delete client;
 }
 
-void on_read(uv_stream_t* tcp, ssize_t nread, const uv_buf_t* buf) {
-    cout << "echo_read(stream=" << tcp << "nread=" << nread << endl;
-    if (nread == UV_EOF) {
-        cout << "<EOF>" << endl;
-    }
-    cout << "Buff" << buf->base << endl;
+void on_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
+    cout << "echo_read(stream=" << client->io_watcher.fd << "nread=" << nread << endl;
+//    extern ssize_t send (int __fd, const void *__buf, size_t __n, int __flags);
+    std::string hello = "HTTP/1.1 200 Ok \r\n";
+    hello += "Server: MLQ/0.1.2\r\n";
+    hello += "Connection: close\r\n";
+    hello += "Date: Fri, 13 Sep 2019 00:57:02 GMT\r\n";
+    hello += "Content-Length: 11\r\n";
+    hello += "Content-Type: text/plain; charset=UTF-8\r\n\r\n";
+    hello += "Hello world\n";
 
-    delete buf->base;
+    if (nread < 0) {
+        cout << "<EOF>" << endl;
+        uv_close((uv_handle_t*) client, on_close);
+    }
+
+    int r = send(client->io_watcher.fd,  hello.c_str(), hello.length(), 0);
+    cout << "sending " << r << endl;
+    uv_close((uv_handle_t*) client, on_close);
+
+    cout << buf->base << endl;
+    delete[] buf->base;
 }
 
 void on_connect(uv_stream_t* server_handle, int status) {
@@ -71,6 +87,6 @@ int main() {
       assert(r == 0);
 
       uv_listen((uv_stream_t*) &server, DEFAULT_BACKLOG, on_connect);
-      cout << "listening on port 8000" << endl;
+      cout << "server started listening on port " << DEFAULT_PORT << endl;
       return uv_run(uv_loop, UV_RUN_DEFAULT);
 }

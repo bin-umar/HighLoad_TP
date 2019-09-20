a74e2fe1f1d14ff7df7e6f2f4271fda230bbac41
abdullah-ibn-umar <jakha_96-96@mail.ru>
2019-09-20 20:26:56 +0300

    deleted this keyword from Classes

--- src/Request.cpp
+++ src/Request.cpp
@@ -13,51 +13,51 @@ void Request::Parse(char* __str) {
     std::getline(stream, token, '\n');
 
     std::istringstream first_line(token);
-    first_line >> this->method >> this->uri >> this->http_version;
-    this->http_version = this->http_version.substr(5);
+    first_line >> method >> uri >> http_version;
+    http_version = http_version.substr(5);
 
-    cout << "method = " << this->method << endl;
-    cout << "uri = " << this->uri << endl;
-    cout << "http_version = " << this->http_version << endl;
+    cout << "method = " << method << endl;
+    cout << "uri = " << uri << endl;
+    cout << "http_version = " << http_version << endl;
 
     while(std::getline(stream, token, '\n')) {
-        if (this->CheckHeader(token, "Host")) {
-            this->set_hostname(token);
-        } else if (this->CheckHeader(token, "Connection")) {
-            this->set_connection_type(token);
-        } else if (this->CheckHeader(token, "User-Agent")) {
-            this->set_user_agent(token);
-        } else if (this->CheckHeader(token, "Accept:")) {
-            this->set_accept(token);
-        } else if (this->CheckHeader(token, "Referer")) {
-            this->set_referer(token);
+        if (CheckHeader(token, "Host")) {
+            set_hostname(token);
+        } else if (CheckHeader(token, "Connection")) {
+            set_connection_type(token);
+        } else if (CheckHeader(token, "User-Agent")) {
+            set_user_agent(token);
+        } else if (CheckHeader(token, "Accept:")) {
+            set_accept(token);
+        } else if (CheckHeader(token, "Referer")) {
+            set_referer(token);
         }
     }
 }
 
 void Request::set_hostname(string& __token) {
-    this->hostname = __token.substr(6);
-    cout << "hostname = " << this->hostname << endl;
+    hostname = __token.substr(6);
+    cout << "hostname = " << hostname << endl;
 }
 
 void Request::set_connection_type(string& __token) {
-    this->connection_type = __token.substr(12);
-    cout << "connection_type = " << this->connection_type << endl;
+    connection_type = __token.substr(12);
+    cout << "connection_type = " << connection_type << endl;
 }
 
 void Request::set_user_agent(string& __token) {
-    this->user_agent = __token.substr(12);
-    cout << "user_agent = " << this->user_agent << endl;
+    user_agent = __token.substr(12);
+    cout << "user_agent = " << user_agent << endl;
 }
 
 void Request::set_accept(string& __token) {
-    this->accept = __token.substr(8);
-    cout << "accept = " << this->accept << endl;
+    accept = __token.substr(8);
+    cout << "accept = " << accept << endl;
 }
 
 void Request::set_referer(string& __token) {
-    this->referer = __token.substr(9);
-    cout << "referer = " << this->referer << endl;
+    referer = __token.substr(9);
+    cout << "referer = " << referer << endl;
 }
 
 bool Request::CheckHeader(string& token, string __header) {
--- src/Response.cpp
+++ src/Response.cpp
@@ -5,15 +5,15 @@
 #include "../include/Response.h"
 
 Response::Response() {
-    this->http_version = "1.1";
-    this->content_length = 0;
-    this->connection_type = "keep-alive";
-    this->server = "Ubuntu(Pop OS)/19.02";
+    http_version = "1.1";
+    content_length = 0;
+    connection_type = "keep-alive";
+    server = "Ubuntu(Pop OS)/19.02";
 }
 
 void Response::Status(int __st) {
-    this->status = std::to_string(__st);
-    this->phrase = status_phrase.at(__st);
+    status = std::to_string(__st);
+    phrase = status_phrase.at(__st);
 }
 
 void Response::Send(int __fd, const string& __filename) {
@@ -25,22 +25,22 @@ void Response::Send(int __fd, const string& __filename) {
 
     for (const auto& type: mime_types) {
         if (__filename.find(type.format) != std::string::npos) {
-            this->connection_type = type.mime;
+            connection_type = type.mime;
         }
     }
 
-    this->content_length = (size_t)fileStat.st_size;
-    this->date = Response::getCurrentDateGMT();
-    this->set_data();
+    content_length = (size_t)fileStat.st_size;
+    date = Response::getCurrentDateGMT();
+    set_data();
 
-    int r = send(__fd, this->data.c_str(), this->get_size(), 0);
+    int r = send(__fd, data.c_str(), get_size(), 0);
     cout << "sending " << r << endl;
-    this->SendFile(__fd, __filename);
+    SendFile(__fd, __filename);
 }
 
 void Response::SendFile(int __fd, const string& __filename) {
     int file = open(__filename.c_str(), O_RDONLY);
-    int sentBytes = 0, remainData = this->content_length;
+    int sentBytes = 0, remainData = content_length;
     off_t offset = 0;
     while ((sentBytes = sendfile(__fd, file, &offset, BUF_SIZE)) > 0) {
         remainData -= sentBytes;
@@ -63,14 +63,14 @@ string Response::getCurrentDateGMT() {
 }
 
 size_t Response::get_size() {
-    return this->data.length();
+    return data.length();
 }
 
 void Response::set_data() {
-    this->data = "HTTP/" + this->http_version + " " + this->status + " " + this->phrase + "\r\n";
-    this->data += "Server: " + this->server + "\r\n";
-    this->data += "Connection: " + this->connection_type + "\r\n";
-    this->data += "Date: " + this->date + "\r\n";
-    this->data += "Content-Length: " + std::to_string(this->content_length) + "\r\n";
-    this->data += "Content-Type: " + this->content_type + "; charset=UTF-8\r\n\r\n";
+    data = "HTTP/" + http_version + " " + status + " " + phrase + "\r\n";
+    data += "Server: " + server + "\r\n";
+    data += "Connection: " + connection_type + "\r\n";
+    data += "Date: " + date + "\r\n";
+    data += "Content-Length: " + std::to_string(content_length) + "\r\n";
+    data += "Content-Type: " + content_type + "; charset=UTF-8\r\n\r\n";
 }

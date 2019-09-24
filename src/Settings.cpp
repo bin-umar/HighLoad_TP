64b19219a9259c222cbe57594f7ee0c9d0b72172
abdullah-ibn-umar <jakha_96-96@mail.ru>
2019-09-24 20:54:59 +0300

    parsing File fixed

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -5,20 +5,18 @@
 #include "../include/Settings.h"
 #include <fstream>
 
-Settings::Settings(): cpu_limit(4), document_root("var/www/html") {};
+Settings::Settings(): cpu_limit(4), document_root("http-test-suite") {};
 
 void Settings::ParseConfigFile(const string& __config_filename) {
-    std::ifstream file(__config_filename);
-    assert(file.is_open() == 0);
+    std::ifstream file;
+    file.open(__config_filename);
 
     string line, field, value;
-//    std::getline(file, line, '\n');
-//    cout << "sdfsdf" << line << endl;
-    while (std::getline(file, line, '\n')) {
+    while (getline(file, line)) {
         std::istringstream stream(line);
         stream >> field >> value;
 
-        cout << field << value;
+        cout << field << " = " << value << endl;
         if (field == "cpu_limit") {
             cpu_limit = std::atoi(value.c_str());
         } else if (field == "document_root") {
